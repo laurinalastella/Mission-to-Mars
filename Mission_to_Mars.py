a83a805c42b3fb20cb8a4d7df915b1748a731877
Laurina LaStella <106928784+laurinalastella@users.noreply.github.com>
2022-08-20 13:33:05 -0700

    Troubleshooting

---
+++ Mission_to_Mars.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('div.list_text')
+
+slide_elem.find('div', class_='content_title')
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+# Mod 10/3/3
+# There are two methods used to find tags and attributes with BeautifulSoup:
+# .find() is used when we want only the first class and attribute we've specified.
+# .find_all() is used when we want to retrieve all of the tags and attributes.
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+
+# ### Featured Images
+
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+# Use the base URL to create an absolute URL
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+# By specifying an index of 0, we're telling Pandas to 
+# pull only the first table it encounters, 
+# or the first item in the list.
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.columns=['description', 'Mars', 'Earth']
+df.set_index('description', inplace=True)
+df
+
+df.to_html()
+
+browser.quit()
+
+# ending at the top of 10.3.6.
+
---
+++ installs.py
@@ -0,0 +1,11 @@
+pip install beautifulsoup4
+pip install bs4
+pip install pymongo
+pip install lxml
+pip install splinter
+pip install webdriver-manager
+pip install selenium
+
+python -m pip install flask
+python -m pip install pymongo
+python -m pip install Flask-Pymongo
